9461b4132d33b062723a6b4dfd5b4b8054414c47
andy-wittig <Awittig@nevada.unr.edu>
2025-07-07 16:02:20 -0700

    initial files

---
+++ main.js
@@ -0,0 +1,12 @@
+  const express = require('express');
+  const app = express();
+
+  app.use
+
+  app.get('/', (req, res) => {
+      res.send('Hello, Express!');
+  });
+
+  app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
+  });
